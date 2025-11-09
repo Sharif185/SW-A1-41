c737bb612449c4edc4d744e1ce3ae8517f25588f
Sharif185 <sharifssentaayi1@gmail.com>
2025-11-09 09:07:26 +0300

    Add setup.sh with OpenCV dependencies and fix imports

---
+++ setup.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+apt-get update
+apt-get install -y libgl1-mesa-glx
+apt-get install -y libglib2.0-0
+apt-get install -y libsm6
+apt-get install -y libxext6
+apt-get install -y libxrender-dev
+apt-get install -y libgl1
